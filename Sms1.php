0778b479cd6b24145a9ee52cfad421f1a0605ba0
both-tool <69092317+both-tool@users.noreply.github.com>
2020-08-02 10:29:31 +0700

    Update Sms1.php

--- Sms1.php
+++ Sms1.php
@@ -1,4 +1,21 @@
-<?php
-$g = "KONTOL BAPAK KAU PECAH";
-echo "$g";
-?>
+Script Spam Sms PizzaHut
+
+#Gunakan dengan bijak tod
+
+
+Penginstalan
+
+
+$ pkg update && pkg upgrade
+$ pkg install php -y
+$ pkg install git -y
+$ git clone https://github.com/KangColly/SMSboomber
+$ cd SMSboomber
+$ php sms1.php
+
+
+
+Oke cuy, ini baru rilis, kalau spam,  ke nomor mantan gw aja, ntar gw kasi😂
+
+
+#habis update gengs
